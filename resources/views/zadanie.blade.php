b5130839c5bd93591d0ae240e647a48697ccbd2e
lacike <ladislav.valient@gmail.com>
2019-05-06 20:24:45 +0200

    Uprava zadania :)

--- resources/views/zadanie.blade.php
+++ resources/views/zadanie.blade.php
@@ -23,51 +23,51 @@
         <h1 class="text-center">Zadanie</h1>
 
         <p>
-                Zadaním je naprogramovať jednoduchý blog, ktorý by mal nasledovné funkcionality:
-            </p>
+            Zadaním je naprogramovať jednoduchý blog, ktorý by mal nasledovné funkcionality:
+        </p>
         <ul>
             <li>vylistovanie zoznamu všetkých blogov</li>
             <li>detailné zobrazenie postu</li>
-            <li>po kliknutí na autora vylistovať zoznam blogov daného autora</li>
+            <li>po kliknutí na autora, vylistovať zoznam blogov daného autora</li>
+            <li>po kliknutí na tag, vylistovať zoznam blogov s danym tagom</li>
         </ul>
 
         <h4>Základné vlastnosti</h4>
         <ul>
             <li>každý blogpost by mal mať:
-                <ul>
-                    <ol>
-                        <li>title</li>
-                        <li>perex (resp. description)</li>
-                        <li>body</li>
-                        <li>autora</li>
-                    </ol>
-                </ul>
-            <br>
+                <ol>
+                    <li>title</li>
+                    <li>perex (resp. description)</li>
+                    <li>body</li>
+                    <li>autora</li>
+                </ol>
+                <br>
             <li>autor by mal mať:
-                <ul>
-                    <ol>
-                        <li>meno</li>
-                        <li>priezvisko</li>
-                    </ol>
-                </ul>
+                <ol>
+                    <li>meno</li>
+                    <li>priezvisko</li>
+                </ol>
             </li>
         </ul>
 
         <h1 class="text-center">Riešenie</h1>
         <h4>FRONTEND</h4>
         <p>
-            Ako základný Layout som použil Temlate na technológii <a href="https://getbootstrap.com">Bootsrtap v4.1</a>, ktorý sa nachádza na
+            Ako základný Layout som použil Temlate na technológii <a href="https://getbootstrap.com">Bootsrtap v4.1</a>,
+            ktorý sa nachádza na
             <a href="https://startbootstrap.com/template-categories/blogs/">Start Bootstrap</a>.
         </p>
         <h4>BACKEND</H4>
         <p>
             Zdrojové kódy su uložené na public repozitári: <a href="https://github.com/Lacike33/MyBlog">GITHUB</a><br>
         </p>
-        <p>Aplikácia je nasadená na živý server za pomoci Heroku a automatického <strong>DEPLOY</strong> a aplikácia je dostupná na <a href="https://mojprvyblog.herokuapp.com">www.mojprvyblog.herokuapp.com</a></p>
+        <p>Aplikácia je nasadená na živý server za pomoci Heroku a automatického <strong>DEPLOY</strong> a aplikácia je
+            dostupná na <a href="https://mojprvyblog.herokuapp.com">www.mojprvyblog.herokuapp.com</a></p>
         <p>Pri tvorbe Blogu sú použité <strong>Accessor</strong> a <strong>Mutator</strong>.</p>
         <p>Testovacie dáta sú generované cez pripravené Factory.</p>
         <p>Základná funkcionalita je rozšírená o Tags, čo užívateľovi umožňuje vylistovať príspevky podľa Tagu.</p>
-        <p>V blogu je možné pridávať nové posty , len registrovaným užívateľom, a navyše Post je možné aj editovať užívateľom, ktorý Post vytvoril.</p>
+        <p>V blogu je možné pridávať nové posty , len registrovaným užívateľom, a navyše Post je možné aj editovať
+            užívateľom, ktorý Post vytvoril.</p>
     </div>
 
 @endsection
